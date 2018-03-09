9acdb42b3a51153fe97395ecb2741ac642039220
Unknown <draggili@gmail.com>
2018-03-09 11:37:54 +0100

    setup synaptic + lambda functions

--- handler.js
+++ handler.js
@@ -1,16 +1,31 @@
 'use strict';
+const synaptic = require('synaptic');
 
-module.exports.hello = (event, context, callback) => {
+const learn = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'You asked the machine to train a model',
       input: event,
     }),
   };
 
   callback(null, response);
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+const predict = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'You asked for a prediction',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
 };
+
+module.exports = {
+  learn: learn,
+  predict: predict
+}
