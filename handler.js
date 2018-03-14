974cb4b0d77f0ca4f8b4cce9fd3e4b2bc532442c
Unknown <draggili@gmail.com>
2018-03-14 15:35:55 +0100

    train network

--- handler.js
+++ handler.js
@@ -1,22 +1,60 @@
 'use strict';
 const synaptic = require('synaptic');
+const Neuron = synaptic.Neuron,
+	Layer = synaptic.Layer,
+	Network = synaptic.Network,
+	Trainer = synaptic.Trainer,
+	Architect = synaptic.Architect;
 const s3Client = require('./lib/s3Client')
 const dataBucketName = process.env.DATA_BUCKET;
 const modelBucketName = process.env.MODEL_BUCKET;
 
 const learn = (event, context, callback) => {
+  let network = new Architect.Perceptron(2, 64, 2)
+  let trainer = new Trainer(network)
+
   s3Client.download(dataBucketName, 'combats.csv')
     .then(data => {
-      console.log(data);
+      console.log('data retrieved from s3')
 
+      data = data.split("\n");
+      data.shift(); // remove header
+      const trainingSet = data.map(line => {
+        let vals = line.split(',');
+        let input = [
+          parseInt(vals[0]) / 800,
+          parseInt(vals[1]) / 800
+        ];
+        let output = (vals[2] == vals[0]) ? [1, 0] : [0, 1]
+        return {
+          input: input,
+          output: output
+        }
+      });
+
+      const options = {
+        iterations: 100,
+        rate: .005,
+      	shuffle: true,
+        schedule: {
+        	every: 10,
+        	do: function(data) {
+        		console.log("iterations", data.iterations, "error", data.error);
+        	}
+        }
+      };
+      console.log('training network')
+      const result = trainer.train(trainingSet, options)
+      let networkJSON = network.toJSON()
 
       const response = {
         statusCode: 200,
         body: JSON.stringify({
-          message: 'You asked the machine to train a model'
+          message: `network was trained over ${result.iterations} iterations in ${result.time}ms. The final error was ${result.error}`
         }),
       };
       callback(null, response);
+
     })
     .catch(err => {
       console.error(err);
