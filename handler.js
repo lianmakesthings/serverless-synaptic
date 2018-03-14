ca070c3884ddaed1f2d8b5f6438cfd78b5e0de49
Unknown <draggili@gmail.com>
2018-03-14 11:02:11 +0100

    load file from s3

--- handler.js
+++ handler.js
@@ -1,24 +1,32 @@
 'use strict';
 const synaptic = require('synaptic');
+const s3Client = require('./lib/s3Client')
+const dataBucketName = process.env.DATA_BUCKET;
+const modelBucketName = process.env.MODEL_BUCKET;
 
 const learn = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'You asked the machine to train a model',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
+  s3Client.download(dataBucketName, 'train.csv')
+    .then(data => {
+      console.log(data);
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'You asked the machine to train a model'
+        }),
+      };
+      callback(null, response);
+    })
+    .catch(err => {
+      console.error(err);
+      callback(new Error('[500] Internal server error'));
+    });
 }
 
 const predict = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'You asked for a prediction',
-      input: event,
+      message: 'You asked for a prediction'
     }),
   };
 
